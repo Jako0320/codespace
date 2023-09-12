2d541a0605e5bb9363f97c151bf0ca4de173b179
fishy <miah.nishat.227@gmail.com>
2023-09-12 19:54:18 -0400

    removed unecessary images

---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './HomepageStyles.css';
+
+
+const HomePage = () => {
+  return (
+    <div>
+      <header>
+        <h1>My Simple React Home Page</h1>
+      </header>
+      <main>
+        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
+        <img src="https://via.placeholder.com/300" alt="Placeholder" />
+      </main>
+    </div>
+  );
+}
+
+export default HomePage;
