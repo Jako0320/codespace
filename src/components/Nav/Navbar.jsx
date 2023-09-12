8dfac1a5111f181e5c43463e745d0b076a17f9a5
fishy <miah.nishat.227@gmail.com>
2023-09-12 18:50:04 -0400

    added styling and made navbar compatible with mobile or smaller screens

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { Component } from 'react'
 import "./NavbarStyles.css";
 
 
-function Navbar() {
+class Navbar extends Component {
+  state={clicked: false};
+  handleClick = () =>{
+    this.setState({clicked: !this.state.clicked})
+  }
+  render(){
   return(
     <>
       <nav>
@@ -12,9 +17,9 @@ function Navbar() {
 
 
         <div>
-          <ul>
+          <ul id="navbar" className={this.state.clicked ? '#navbar active' : "#navbar"}>
             <li>
-              <a href='index.html'>Home</a>
+              <a className='active' href='index.html'>Home</a>
             </li>
             <li>
               <a href='index.html'>Calendar</a>
@@ -30,11 +35,16 @@ function Navbar() {
             </li> 
           </ul>
         </div>
+
+        <div id="mobile" onClick={this.handleClick}>
+         <i id="bar" className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+        </div>
+    
       </nav>
     </>
   )
 }
-
+}
 
 
 export default Navbar;
