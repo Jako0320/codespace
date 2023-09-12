1ac8ffbf9859d61c19497336ee6e029d7f302671
fishy <miah.nishat.227@gmail.com>
2023-09-12 19:44:11 -0400

    added footer to homepage and connected to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Nav/Navbar';
-
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <>
      <Navbar/>
       <h1>Hello World</h1>
+     <Footer/>
     </>
   );
 }
