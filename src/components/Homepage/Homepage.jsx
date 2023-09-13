54a96f3bfbb43b989d14002bd05b8cf7a4f18bdd
fishy <miah.nishat.227@gmail.com>
2023-09-13 00:06:17 -0400

    begin building homepage

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -4,15 +4,12 @@ import './HomepageStyles.css';
 
 const HomePage = () => {
   return (
-    <div>
-      <header>
-        <h1>My Simple React Home Page</h1>
-      </header>
-      <main>
-        <p>Welcome to my simple React home page! This is a basic example of a React project.</p>
-        <img src="https://via.placeholder.com/300" alt="Placeholder" />
-      </main>
-    </div>
+  <>
+  <div className='section1'>
+    <img src='/spaceBg.jpg'></img>
+  </div>
+
+  </>
   );
 }
 
