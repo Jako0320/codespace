425cd997a79faf71a706e8198fd1130acb031797
fishy <miah.nishat.227@gmail.com>
2023-09-14 21:43:47 -0400

    re-added homepage.jsx and css file

---
+++ client/src/components/Homepage/Homepage.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import './HomepageStyles.css';
+
+
+const HomePage = () => {
+  return (
+  <>
+  <div className='section1'>
+    <img src='/spaceBg.jpg'></img>
+  </div>
+
+  </>
+  );
+}
+
+export default HomePage;
