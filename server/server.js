718b76e9246c0ddeb6bcd9b49a6aee05f5e14e13
Jacques <jacques.scoffoni@gmail.com>
2023-09-19 18:26:47 -0400

    Bug corrections

--- server/server.js
+++ server/server.js
@@ -1,59 +1,48 @@
 const express = require('express');
-// const { ApolloServer } = require('@apollo/server');
-// const { expressMiddleware } = require('@apollo/server/express4');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
-// const { typeDefs, resolvers } = require('./schemas');
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-// const startApolloServer = async () => {
-//   await server.start();
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
 
-  app.use(express.urlencoded({
-    extended: true
-  }));
+  app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  // app.use('/graphql', expressMiddleware(server, {
-  //   context: authMiddleware
-  // }));
+  // Serve up static assets
+  app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-  // mongodb 
-  const MongodbRouter = require('./routes/mongodb/index')
-   app.use('/mongodb',MongodbRouter)
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
 
-  // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-// }
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     });
   });
 };
 
-// Call function to start server
-// startApolloServer();
-
-
-
-app.get('/', (req, res) => {
-  res.send('Backend for codespace');
-});
-
-app.listen(3000, () => console.log('App is listening on port 3000.'));
+// Call the async function to start the server
+startApolloServer();
