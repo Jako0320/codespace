15f8a80f196e5e433064a69c7dbd7ed817dddda9
Jacques <jacques.scoffoni@gmail.com>
2023-09-18 20:54:51 -0400

    Auth code addition to main

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import Auth from "./utils/auth";
+import { setContext } from "@apollo/client/link/context";
 import "./App.css";
 import Navbar from "./components/Nav/Navbar";
 import Footer from "./components/Footer/Footer";
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
@@ -28,13 +29,17 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-
 function App() {
+  const token = Auth.getToken();
+  // If the token is somehow in localStorage but expired remove it
+  if (token && Auth.isTokenExpired(token)) {
+    localStorage.removeItem("id_token");
+  }
   return (
     <ApolloProvider client={client}>
-        <Navbar />
-        <Outlet />
-        <Footer />
+      <Navbar />
+      <Outlet />
+      <Footer />
     </ApolloProvider>
   );
 }
