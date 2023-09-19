32d0f1819021ab8e2e1ff6a3156469173ba4b5d7
kevluck <kevinl8700@gmail.com>
2023-09-18 20:13:12 -0400

    donate page added

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,11 @@
 const Event = require('./Event');
 const User = require('./User');
 const UserEvent = require('./UserEvent');
+import React from "react";
+import ReactDOM from "react-dom";
+import DonationPage from "./DonationPage";
+
+ReactDOM.render(<DonationPage />, document.getElementById("root"));
+
 
 module.exports = { Event, User, UserEvent };
