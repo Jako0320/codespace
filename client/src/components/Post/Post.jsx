a8fbde277938cf9973e35ed9c27d422184eb9a3b
Jacques <jacques.scoffoni@gmail.com>
2023-09-19 03:33:20 -0400

    Forum and post history

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,90 +1,31 @@
-import React from 'react';
-import  './PostStyles.css';
-import profileUserImg from '../../assets/profile1.jpg'
-import {HiOutlineDotsVertical} from 'react-icons/hi'
-import {AiOutlineHeart} from 'react-icons/ai'
-import {BiMessageRounded} from 'react-icons/bi'
-import {BsBookmark} from 'react-icons/bs'
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { CREATE_POST_MUTATION } from '../../utils/mutations';
 
 
 const Post = () => {
+  const [content, setContent] = useState('');
+  const [createPost] = useMutation(CREATE_POST_MUTATION);
+
+  const handlePostSubmit = async () => {
+    try {
+      const { data } = await createPost({
+        variables: { content },
+      });
+    } catch (error) {
+    }
+  };
+
   return (
-    <div className={classes.container}>
-        <div className={classes.wrapper}>
-            <div className={classes.top}>
-              <div className={classes.topLeft}>
-              <img src={profileUserImg} className={classes.profileUserImg}/>
-                <div className={classes.profileMetadata}>
-                    <span>WebDevMania</span>
-                    <span>3 hours ago</span>
-                </div>
-              </div>
-             <HiOutlineDotsVertical size={25}/>
-            </div>
-            <div className={classes.center}>
-                <div className={classes.desc}>My first post!</div>
-                <img src={profileUserImg} className={classes.postImg} />
-            </div>
-            <div className={classes.controls}>
-                <div className={classes.controlsLeft}>
-                    <AiOutlineHeart />
-                    <BiMessageRounded />
-                </div>
-                <div className={classes.controlsRight}>
-                    <BsBookmark />
-                </div>
-            </div>
-            <div className={classes.comments}>
-                <div className={classes.comment}>
-                    <div className={classes.commentLeft}>
-                      <img src={profileUserImg} className={classes.commentImg}/>
-                      <div className={classes.commentData}>
-                        <span>WebDevMania</span>
-                        <span className={classes.commentTimeago}>1 hour ago</span>
-                      </div>
-                      <div className={classes.commentText}>Nice post WebDevMania!</div>
-                    </div>
-                    <div className={classes.commentRight}>
-                        <AiOutlineHeart />
-                        <span>5 likes</span>
-                    </div>
-                </div>
-                <div className={classes.comment}>
-                    <div className={classes.commentLeft}>
-                      <img src={profileUserImg} className={classes.commentImg}/>
-                      <div className={classes.commentData}>
-                        <span>WebDevMania</span>
-                        <span className={classes.commentTimeago}>1 hour ago</span>
-                      </div>
-                      <div className={classes.commentText}>Nice post WebDevMania!</div>
-                    </div>
-                    <div className={classes.commentRight}>
-                        <AiOutlineHeart />
-                        <span>5 likes</span>
-                    </div>
-                </div>
-                <div className={classes.comment}>
-                    <div className={classes.commentLeft}>
-                      <img src={profileUserImg} className={classes.commentImg}/>
-                      <div className={classes.commentData}>
-                        <span>WebDevMania</span>
-                        <span className={classes.commentTimeago}>1 hour ago</span>
-                      </div>
-                      <div className={classes.commentText}>Nice post WebDevMania!</div>
-                    </div>
-                    <div className={classes.commentRight}>
-                        <AiOutlineHeart />
-                        <span>5 likes</span>
-                    </div>
-                </div>
-            </div>
-            <div className={classes.postCommentSection}>
-                <input type="text" className={classes.inputSection} placeholder='Type here...'/>
-                <button>Post</button>
-            </div>
-        </div>
+    <div>
+      <textarea
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        placeholder="Write your post here..."
+      />
+      <button onClick={handlePostSubmit}>Submit</button>
     </div>
-  )
-}
+  );
+};
 
 export default Post;
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,16 +1,41 @@
-const { User, Event } = require('../models');
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { User, Event, Post } = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
+    getPosts: async () => {
+      try {
+        // Fetch all posts from the database
+        const posts = await Post.find().sort({ createdAt: -1 });
+        return posts;
+      } catch (error) {
+        throw new Error("Failed to fetch posts");
+      }
+    },
+    getUserPosts: async (_, __, context) => {
+      // Check if the user is authenticated
+      if (!context.user) {
+        throw new AuthenticationError('User not authenticated');
+      }
+
+      try {
+        // Fetch the posts associated with the authenticated user
+        const userPosts = await Post.find({ author: context.user._id }).sort({
+          createdAt: -1,
+        });
+        return userPosts;
+      } catch (error) {
+        throw new Error('Failed to fetch user posts');
+      }
+    },    
     me: async (parent, args, context) => {
       if (context.user) {
-          return User.findOne({
-              _id: context.user_id
-          });
+        return User.findOne({
+          _id: context.user._id,
+        });
       }
-      throw AuthenticationError;
-    }, 
+      throw new AuthenticationError("User not authenticated");
+    },
   },
 
   Mutation: {
@@ -18,56 +43,75 @@ const resolvers = {
       const user = await User.findOne({ email: email });
 
       if (!user) {
-          throw AuthenticationError;
+        throw new AuthenticationError("User not found");
       }
 
       const correctPw = await user.isCorrectPassword(password);
 
       if (!correctPw) {
-          throw AuthenticationError;
+        throw new AuthenticationError("Incorrect password");
       }
 
       const token = signToken(user);
       return { token, user };
     },
 
-    addUser: async (parent, { firstName, lastName, email, password }) => {
-      const user = await User.create({ firstName, lastName, email, password });
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
       const token = signToken(user);
 
-      return {token, user };
+      return { token, user };
+    },
+
+    createPost: async (_, { content }, context) => {
+      const author = context.user;
+
+      try {
+        // Create a new post in the database
+        const newPost = new Post({
+          content,
+          author,
+          createdAt: new Date().toISOString(),
+        });
+
+        const post = await newPost.save();
+        return post;
+      } catch (error) {
+        throw new Error("Failed to create a new post");
+      }
     },
 
     saveEvent: async (parent, args, context) => {
       if (context.user) {
         const user = await User.findOneAndUpdate(
-            { _id: context.user._id },
-            { $addToSet: { savedEvents: args.eventToSave }
-            },
-            { new: true }
+          { _id: context.user._id },
+          {
+            $addToSet: { savedEvents: args.eventToSave },
+          },
+          { new: true }
         );
-        
+
         return user;
-    }
-    throw AuthenticationError;
-  },
+      }
+      throw new AuthenticationError("User not authenticated");
+    },
 
     removeEvent: async (parent, args, context) => {
       if (context.user) {
         return User.findOneAndUpdate(
-            { _id: context.user._id },
-            { 
-                $pull: {
-                savedEvents: {
-                    eventId: args.deleteEvent
-                    },
-                },
+          { _id: context.user._id },
+          {
+            $pull: {
+              savedEvents: {
+                eventId: args.deleteEvent,
+              },
             },
-            { new: true }
+          },
+          { new: true }
         );
       }
-      throw AuthenticationError;
-    }
+      throw new AuthenticationError("User not authenticated");
+    },
   },
 };
 
